4ec241d0b859d717ed665ff8a3586fac4c0562f3
Peter Tempfli <tempflip@gmail.com>
2016-12-06 19:12:00 +0100

    first working v

--- REST-API/Javascript-SDK/p360-sdk.js
+++ REST-API/Javascript-SDK/p360-sdk.js
@@ -1,16 +1,67 @@
+var $_jq = jQuery.noConflict();
+
 var Payment360 = {
+
+	// Properties
+	publishableKey : null,
+	endpoint : null,
 
 	// objects
 	//////////
 
 	PaymentMethod : function() {
 		this.paymentGatewayId = null;
-		this.publishableKey = null;
 		this.customerId = null;
 		this.accountId = null;
 		this.stripePayload = null;
 		this.email = null;
 		this.transactionList = [];
+
+		/*
+		* This is a call for the REST endpoint.
+		*/
+		this._createPaymentMethod = function(successCallback, failCallback) {
+			$_jq('.p360-spinner').show();
+
+			var d = {
+				action : 'createPaymentMethod',
+				publishableKey : Payment360.publishableKey,
+				customerId : this.customerId,
+				accountId : this.accountId,
+				stripePayload : this.stripePayload,
+				email : this.email,
+				transactionList : this.transactionList
+			};
+
+			var that = this;
+
+			var evalClosure = function() {
+				return function(r) {
+					$_jq('.p360-spinner').hide();
+					that._evalAPIReturn(r, successCallback, failCallback);
+				};
+			};
+
+            $_jq.ajax({
+                type : 'POST',
+                url : Payment360.endpoint,
+                headers : {
+                    "content-type": "text/plain"
+                },
+                data : JSON.stringify(d),
+                success : evalClosure()
+            }); 
+
+		};
+
+		this._evalAPIReturn = function(r, successCallback, failCallback) {
+			if (r.success === true) {
+				successCallback(r);
+			} else {
+				failCallback(r);
+			}
+		};
+
 	},
 
 	Transaction : function() {
@@ -20,22 +71,32 @@ var Payment360 = {
 		this.openOnly = null;
 	},
 
-	// methods
+	// public methods
 	//////////
 
+
+	/*
+	* Sets the publishable key globally. This is required in order to do any action
+	*/
 	setPublishableKey : function(publishableKey) {
 		Payment360.publishableKey = publishableKey;
 	},
 
-	popStripeCheckout : function(name, description, amount, image, callback) {
-		var tokenCallback = function() {
+	setEndpoint : function(endpoint) {
+		Payment360.endpoint = endpoint;
+	},
 
-		};
-	
+	/*
+	* Pops the Stripe Checkout form and charges the card immediatally.
+	*/
+	popStripeCheckout : function(name, description, amount, image, successCallback, failCallback) {
+		
 		var handler = new StripeCheckout.configure({
 			key: Payment360.publishableKey,
 			locale: 'auto',
-			token: tokenCallback
+			token: function(payload) {
+				Payment360._chargePayload(payload, amount, successCallback, failCallback);
+			}
 		});
 
 		handler.open({
@@ -46,12 +107,27 @@ var Payment360 = {
 			billingAddress : true,	
 		});
 
-
 	},
 
-	createPaymentMethod: function(stripePayload, transactions) {
+	// private methods
+	//////////////////
+	_chargePayload : function(payload, amount, successCallback, failCallback) {
+		var tra = new Payment360.Transaction();
+		tra.amount = amount;
 
-	}
+		var pm = new Payment360.PaymentMethod();
+		pm.publishableKey = this.publishableKey;
+		pm.stripePayload = JSON.stringify(payload);
+		pm.transactionList.push(tra);
+
+		pm._createPaymentMethod(successCallback, failCallback);
+	},
 
 };
+
+var _p360init = function() {
+	$_jq('.p360-spinner').hide();
+};
+
+$_jq(document).ready(_p360init);
 
